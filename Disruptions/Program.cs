6dcfd7b54099aea83964dbd25ec942b20e6d9c7d
Todor <84274078+TLizhev@users.noreply.github.com>
2024-10-20 12:00:54 +0100

    Add htmlagility and get html for disruptions delays and elizabeth line.

--- Disruptions/Program.cs
+++ Disruptions/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+
+var url = "https://tfl.gov.uk/tube-dlr-overground/status/";
+var web = new HtmlWeb();
+var doc = web.Load(url);
+
+var lizzie = doc.DocumentNode.SelectNodes("//div[@class='rainbow-list-content']").Where(x => x.OuterHtml.Contains("elizabeth"));
+var disruptions = doc.DocumentNode.SelectNodes("//div[@class='rainbow-list-content']").Where(x => x.OuterHtml.Contains("disruption"));
+var delays = doc.DocumentNode.SelectNodes("//div[@class='rainbow-list-content']").Where(x => x.OuterHtml.Contains("delay"));
